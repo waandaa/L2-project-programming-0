43c4057cf65cd4183d26c3cc0968459a07a1f4cb
waandaa <wandahopkinson@mmc.school.nz>
2025-05-28 10:10:31 +1200

    Name check age check pay check all  together basic

---
+++ C_05_name_age_pay.py
@@ -0,0 +1,86 @@
+# Functions go here
+def int_check(question, low, high):
+    """Checks users enter an integer between two values"""
+
+    error = f"Oops - please enter an integer between {low} and {high}."
+
+    while True:
+
+        try:
+            # Change the response to an integer and check that it's more than zero
+            response = int(input(question))
+
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again.\n")
+
+
+def string_check(question, valid_answers=('yes', 'no'),
+                 num_letters=1):
+    """Checks that users enter the full word
+       or the 'n' letter/s of a word from a list of valid responses
+       :rtype: object"""
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_answers:
+
+            # check if the response is the entire world
+            if response == item:
+                return item
+
+            # check if it's the 'n' letters
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"Please choose an option from {valid_answers}")
+
+
+# Main Routine goes here
+
+# intialise variables / non-default options for string checker
+payment_ans = ('cash', 'credit')
+
+#loop for testing purposes...
+while True:
+    print()
+
+    # ask user for their name (and check it's not blank)
+    name = not_blank("Name: ")
+
+    # Ask for their age and check it's between 12 and 120
+    age = int_check("Age: ", 10, 121)
+
+    # Output error message / success message
+    if age < 12:
+        print(f"{name} is too young")
+        continue
+    elif age > 120:
+        print(f"{name} is too old")
+        continue
+    else:
+        pass
+
+    # ask user for payment method (cash / credit / ca / cr)
+    pay_method = string_check("Payment method: ", payment_ans, 2)
+    print(f"{name} has bought a ticket ({pay_method})")
+
+
